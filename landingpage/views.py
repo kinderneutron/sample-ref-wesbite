236cf64e1fcf69ccfb88530293eca7da31254986
Suhas S Bhandary <bhandarysuhas777@gmail.com>
2024-03-21 02:01:28 +0530

    Sample Code for IOT Wesbite

---
+++ landingpage/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def landingpageres(request):
+     template = loader.get_template('landing.html')
+     return HttpResponse(template.render())
